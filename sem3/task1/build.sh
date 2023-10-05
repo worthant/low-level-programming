2623283a8f243cc1b9aaf3b9c20ae69444c86d57
worthant <b_dvorkin@niuitmo.ru>
2023-10-05 15:29:39 +0300

    :wrench: Fix build.sh

--- sem3/task1/build.sh
+++ sem3/task1/build.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Assemble the program
-nasm -f elf64 -o declare_str.o declare_str.asm
-
-# Link the object file to create the executable
-ld -o declare_str declare_str.o
+# Preprocess the program
+nasm -e declare_str.asm
 
 # Check the exit status to see if the commands were successful
 if [ $? -eq 0 ]; then
-    echo "Build successful."
+    echo "Preprocess successful."
 else
-    echo "Build failed."
+    echo "Preprocess failed."
 fi
