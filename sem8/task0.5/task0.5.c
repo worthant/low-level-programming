b8d52f54bdc340f41fe0ab6dac274a9a12606077
worthant <b_dvorkin@niuitmo.ru>
2023-12-12 16:19:44 +0300

    task0.5 & task1

--- sem8/task0.5/task0.5.c
+++ sem8/task0.5/task0.5.c
@@ -2,34 +2,33 @@
 #include <stdio.h>
 #include <unistd.h>
 
-pthread_mutex_t m1;
+pthread_mutex_t lock;
 
-void bad_print(char *s)
-{
-    for (; *s != '\0'; s++)
-    {
+void bad_print(char *s) {
+    pthread_mutex_lock(&lock);
+    for (; *s != '\0'; s++) {
         fprintf(stdout, "%c", *s);
         fflush(stdout);
         usleep(100);
     }
+    pthread_mutex_unlock(&lock);
 }
 
-void* my_thread(void* arg)
-{
-    for(int i = 0; i < 10; i++ )
-    {
-        pthread_mutex_lock(&m1);
-        bad_print((char *)arg);
-        pthread_mutex_unlock(&m1);
+void *my_thread(void *arg) {
+    for (int i = 0; i < 10; i++) {
+        bad_print((char *) arg);
         sleep(1);
     }
     return NULL;
 }
+
 int main(void) {
     pthread_t t1, t2;
-    pthread_mutex_init(&m1, NULL);
-    pthread_create(&t1, NULL, my_thread, "girls sometimes\n");
-    pthread_create(&t2, NULL, my_thread, "may be beautiful...\n");
-    pthread_mutex_destroy(&m1);
-    pthread_exit( NULL );
+    pthread_mutex_init(&lock, NULL);
+    pthread_create(&t1, NULL, my_thread, "life is..\n");
+    pthread_create(&t2, NULL, my_thread, "when you're in love with something and someone at the same time\n");
+    pthread_mutex_destroy(&lock);
+    pthread_exit(NULL);
+
+    return 0;
 }
--- sem8/task1/task1.c
+++ sem8/task1/task1.c
@@ -1,56 +1,44 @@
-/* fork-example-1.c */
-#define ARR_SIZE 10
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
+#include <sys/wait.h>
 #include <string.h>
 #include <unistd.h>
+
 void* create_shared_memory(size_t size) {
-    return mmap(NULL,
-                size,
-                PROT_READ | PROT_WRITE,
-                MAP_SHARED | MAP_ANONYMOUS,
-                -1, 0);
+    return mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 }
 
-void print_array(int* arr, size_t size) {
-    for (size_t i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
+int main() {
+    int* shmem = (int*) create_shared_memory(10 * sizeof(int));
+
+    // Заполним общую память числами от 1 до 10
+    fputs("Initial array:", stdout);
+    for (int i = 0; i < 10; i++) {
+        shmem[i] = i + 1;
+        printf(" %d", i);
     }
     puts("\n");
-}
 
-void change_array_element(size_t index, int new_value, int* arr, size_t size) {
-    if (index < size) {
-        arr[index] = new_value;
-    }
-}
-
-int main(void) {
-
-    int nums[] = {1,2,3,4,5,6,7,8,9,10};
-
-    int* shmem = create_shared_memory(sizeof(int)*10);
-    for (size_t i = 0; i < ARR_SIZE; i++) {
-        shmem[i] = nums[i];
-    }
-    printf("Shared memory at: %p\n" , (void*)shmem);
     int pid = fork();
+
     if (pid == 0) {
-        puts("[child] enter index and new value: ");
-        size_t index;
-        int num;
-        scanf("%zu %d", &index, &num);
-        change_array_element(index, num, shmem, ARR_SIZE);
-        puts("[child] finishing child process...");
+        // Дочерний процесс
+        printf("[child] Enter index and new value: ");
+        int index, value;
+        scanf("%d %d", &index, &value);
+        if (index >= 0 && index < 10) {
+            shmem[index] = value;
+        }
+        exit(0);
     } else {
-        printf("[child] Child's pid is: %d\n", pid);
-        puts("[parent] array before: ");
-        print_array(shmem, ARR_SIZE);
-        puts("[parent] wait for child process to dead");
-        wait(NULL);
-        puts("[parent] array after: ");
-        print_array(shmem, ARR_SIZE);
+        // Родительский процесс
+        wait(NULL); // Ждем завершения дочернего процесса
+        printf("[parent] Updated array: ");
+        for (int i = 0; i < 10; i++) {
+            printf("%d ", shmem[i]);
+        }
+        printf("\n");
     }
     return 0;
 }
